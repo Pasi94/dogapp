b12889892fb854d8080f9cd747f9af61b432851d
Pasindu Purna Uduwila Arachchi <ppurnaua@gmail.com>
2017-12-15 17:01:46 +0530

    correction with dog image

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule, MatListModule],
-    exports: [MatButtonModule, MatToolbarModule, MatListModule],
+    imports: [MatButtonModule, MatToolbarModule, MatListModule,MatGridListModule],
+    exports: [MatButtonModule, MatToolbarModule, MatListModule,MatGridListModule],
 })
 export class MaterialModule { }
