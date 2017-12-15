69862c6d81f1509c24b87be529300d01533c2ac7
Pasindu Purna Uduwila Arachchi <ppurnaua@gmail.com>
2017-12-15 17:32:54 +0530

    correction with design

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule, MatListModule,MatGridListModule],
-    exports: [MatButtonModule, MatToolbarModule, MatListModule,MatGridListModule],
+    imports: [MatButtonModule, MatToolbarModule, MatListModule, MatGridListModule, MatTableModule, MatFormFieldModule],
+    exports: [MatButtonModule, MatToolbarModule, MatListModule, MatGridListModule, MatTableModule, MatFormFieldModule],
 })
 export class MaterialModule { }
